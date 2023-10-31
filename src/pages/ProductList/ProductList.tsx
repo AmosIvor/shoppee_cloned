3eec8a121d3fe701548b12fed0b9b9677e1a2af8
AmosIvor <trantuanvu00000@gmail.com>
2023-10-31 09:44:15 +0700

    handle display smart pagination

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -4,9 +4,12 @@ import Product from './Product'
 import SortProductList from './SortProductList'
 import useQueryParams from 'src/hooks/useQueryParams'
 import productApi from 'src/apis/product.api'
+import Pagination from 'src/components/Pagination'
+import { useState } from 'react'
 
 export default function ProductList() {
   const queryParams = useQueryParams()
+  const [page, setPage] = useState(1)
   // console.log('queryParams', queryParams)
   const { data } = useQuery({
     queryKey: ['products', queryParams],
@@ -33,6 +36,7 @@ export default function ProductList() {
                   </div>
                 ))}
             </div>
+            <Pagination page={page} setPage={setPage} pageSize={20} />
           </div>
         </div>
       </div>
